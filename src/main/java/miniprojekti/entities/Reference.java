fba2eb3fd82683dc89aa6dd6dfe4ac78a10325e0
Helena Parviainen <helena.m.parviainen@gmail.com>
2017-04-05 15:02:45 +0300

    reference muutokset

--- src/main/java/miniprojekti/entities/Reference.java
+++ src/main/java/miniprojekti/entities/Reference.java
@@ -17,26 +17,113 @@ public class Reference {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
+    
+    String author;
+    String journal;
+    String year;
+    String volume;
+    String number;
+    String pages;
+    String month;
+    String note;
+    String key;
     String type;
-
-    @OneToMany(cascade = CascadeType.ALL)
-    private List<RefField> fields;
 
     public Reference() {
         
     }
-    
-    public Reference(String type, List<RefField> fields) {
+
+    public Reference(String author, String journal, String year, String volume, String number, String pages, String month, String note, String key, String type) {
+        this.author = author;
+        this.journal = journal;
+        this.year = year;
+        this.volume = volume;
+        this.number = number;
+        this.pages = pages;
+        this.month = month;
+        this.note = note;
+        this.key = key;
         this.type = type;
-        this.fields = fields;
     }
-    
+
     public Long getId() {
         return id;
     }
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getJournal() {
+        return journal;
+    }
+
+    public void setJournal(String journal) {
+        this.journal = journal;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getVolume() {
+        return volume;
+    }
+
+    public void setVolume(String volume) {
+        this.volume = volume;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getPages() {
+        return pages;
+    }
+
+    public void setPages(String pages) {
+        this.pages = pages;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
     }
 
     public String getType() {
@@ -46,12 +133,5 @@ public class Reference {
     public void setType(String type) {
         this.type = type;
     }
-
-    public List<RefField> getFields() {
-        return fields;
-    }
-
-    public void setFields(List<RefField> fields) {
-        this.fields = fields;
-    }
+    
 }
