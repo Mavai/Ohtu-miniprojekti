1b4348263e0edb9b58054d5f2311292c0d5e595b
hanranti <hanranti@cs.helsinki.fi>
2017-04-05 16:49:21 +0300

    Add book reference page content test

--- src/test/java/references/Stepdefs.java
+++ src/test/java/references/Stepdefs.java
@@ -9,23 +9,42 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 /**
  *
  * @author hanranti
  */
 public class Stepdefs {
-    
+
     WebDriver driver = new ChromeDriver();
     String baseUrl = "http://localhost:8080/";
-    
-    @Given("^index is selected$")
-    public void index_is_selected() throws Throwable {
+
+    @Given("^index is visited$")
+    public void index_is_visited() throws Throwable {
         driver.get(baseUrl);
     }
-    
+
+    @When("^article is selected$")
+    public void article_is_selected() throws Throwable {
+        WebElement selectElement = driver.findElement(By.name("type"));
+        Select select = new Select(selectElement);
+        select.selectByVisibleText("book");
+
+        WebElement submit = driver.findElement(By.id("submit"));
+        submit.submit();
+    }
+
     @Then("^page displays create a new reference$")
     public void page_displays_create_a_new_reference() throws Throwable {
         assertTrue(driver.getPageSource().contains("Create a new reference"));
+    }
+
+    @Then("^page displays add a book reference content$")
+    public void page_displays_add_a_book_reference_content() throws Throwable {
+        assertTrue(driver.getPageSource().contains("author"));
+        assertTrue(driver.getPageSource().contains("title"));
+        assertTrue(driver.getPageSource().contains("year"));
+        assertTrue(driver.getPageSource().contains("publisher"));
     }
 }
--- src/test/resources/references/references.feature
+++ src/test/resources/references/references.feature
@@ -1,5 +1,10 @@
 Feature: References page displays right content
 
     Scenario: References page displays Create a new reference
-       Given index is selected
+       Given index is visited
        Then  page displays create a new reference
+
+    Scenario: Add book reference page is accessible from index page
+       Given index is visited
+       When  article is selected
+       Then  page displays add a book reference content
