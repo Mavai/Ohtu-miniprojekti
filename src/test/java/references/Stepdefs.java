078b47468b492741d86949ce2975a55b29805807
hanranti <hanranti@cs.helsinki.fi>
2017-04-06 12:48:26 +0300

    refactor + article tests

--- src/test/java/references/Stepdefs.java
+++ src/test/java/references/Stepdefs.java
@@ -18,25 +18,22 @@ import org.openqa.selenium.support.ui.Select;
 public class Stepdefs {
 
     WebDriver driver = new ChromeDriver();
-    String baseUrl = "http://localhost:8080/";
+    String referencesPath = "http://localhost:8080/references";
 
     @Given("^index is visited$")
     public void index_is_visited() throws Throwable {
-        driver.get(baseUrl);
+        driver.get(referencesPath);
     }
 
-    @When("^book is selected$")
-    public void book_is_selected() throws Throwable {
+    @When("^type: \"([^\"]*)\" is selected$")
+    public void type_is_selected(String type) throws Throwable {
         WebElement selectElement = driver.findElement(By.name("type"));
         Select select = new Select(selectElement);
-        select.selectByVisibleText("book");
-
-        WebElement submit = driver.findElement(By.id("submit"));
-        submit.submit();
+        select.selectByVisibleText(type);
     }
 
-    @When("^form is filled with a value: \"([^\"]*)\" for \"([^\"]*)\"$")
-    public void form_is_filled_with_a_value(String value, String field) throws Throwable {
+    @When("^form is filled with value: \"([^\"]*)\" for \"([^\"]*)\"$")
+    public void form_is_filled_with_value(String value, String field) throws Throwable {
         WebElement fieldElement = driver.findElement(By.id(field));
         fieldElement.sendKeys(value);
     }
@@ -47,21 +44,21 @@ public class Stepdefs {
         submit.submit();
     }
 
-    @Then("^page displays create a new reference$")
-    public void page_displays_create_a_new_reference() throws Throwable {
-        assertTrue(driver.getPageSource().contains("Create a new reference"));
+//    @Then("^page displays create a new reference$")
+//    public void page_displays_create_a_new_reference() throws Throwable {
+//        assertTrue(driver.getPageSource().contains("Create a new reference"));
+//    }
+
+    @Then("^page displays content: \"([^\"]*)\"$")
+    public void page_displays_content(String content) throws Throwable {
+        assertTrue(driver.getPageSource().contains(content));
     }
 
-    @Then("^page displays value: \"([^\"]*)\"$")
-    public void page_displays_value(String field) throws Throwable {
-        assertTrue(driver.getPageSource().contains(field));
-    }
-
-    @Then("^page displays add a book reference content$")
-    public void page_displays_add_a_book_reference_content() throws Throwable {
-        assertTrue(driver.getPageSource().contains("author"));
-        assertTrue(driver.getPageSource().contains("title"));
-        assertTrue(driver.getPageSource().contains("year"));
-        assertTrue(driver.getPageSource().contains("publisher"));
-    }
+//    @Then("^page displays add a book reference content$")
+//    public void page_displays_add_a_book_reference_content() throws Throwable {
+//        assertTrue(driver.getPageSource().contains("author"));
+//        assertTrue(driver.getPageSource().contains("title"));
+//        assertTrue(driver.getPageSource().contains("year"));
+//        assertTrue(driver.getPageSource().contains("publisher"));
+//    }
 }
--- src/test/resources/references/references.feature
+++ src/test/resources/references/references.feature
@@ -2,16 +2,52 @@ Feature: References page displays right content
 
     Scenario: References page displays Create a new reference
        Given index is visited
-       Then  page displays create a new reference
+       Then  page displays content: "Create a new reference"
 
     Scenario: Add book reference page is accessible from index page
        Given index is visited
-       When  book is selected
-       Then  page displays add a book reference content
+       When  type: "book" is selected
+       And   form is submitted
+       Then  page displays content: "author"
+       And   page displays content: "title"
+       And   page displays content: "year"
+       And   page displays content: "publisher"
 
     Scenario: References page displays added book
        Given index is visited
-       When book is selected
-       And form is filled with a value: "testAuthor" for "author"
+       When type: "book" is selected
+       And form is submitted
+       And form is filled with value: "testAuthor" for "author"
+       And form is filled with value: "testTitle" for "title"
+       And form is filled with value: "1955" for "year"
+       And form is filled with value: "testPublisher" for "publisher"
        And form is submitted
        Then page displays value: "testAuthor"
+       And page displays value: "testTitle"
+
+    Scenario: Add article reference page is accessible from index page
+       Given index is visited
+       When  type: "article" is selected
+       And   form is submitted
+       Then  page displays content: "author"
+       And   page displays content: "title"
+       And   page displays content: "year"
+       And   page displays content: "publisher"
+       And   page displays content: "journal"
+       And   page displays content: "volume"
+       And   page displays content: "pages"
+
+    Scenario: References page displays added article
+       Given index is visited
+       When type: "article" is selected
+       And form is submitted
+       And form is filled with value: "testAuthor" for "author"
+       And form is filled with value: "testTitle" for "title"
+       And form is filled with value: "1955" for "year"
+       And form is filled with value: "testPublisher" for "publisher"
+       And form is filled with value: "testJournal" for "journal"
+       And form is filled with value: "1" for "volume"
+       And form is filled with value: "175" for "pages"
+       And form is submitted
+       Then page displays value: "testAuthor"
+       And page displays value: "testTitle"
