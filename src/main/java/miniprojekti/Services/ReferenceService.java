740304585fd42486b956070048931c28a514b1a1
Mavai <marko.vainio14@gmail.com>
2017-05-02 18:02:23 +0300

    refaktorointia

--- src/main/java/miniprojekti/Services/ReferenceService.java
+++ src/main/java/miniprojekti/Services/ReferenceService.java
@@ -9,6 +9,9 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * Created by markovai on 2.5.2017.
  */
@@ -26,7 +29,7 @@ public class ReferenceService {
         return bibtex;
     }
 
-    public String save(Reference reference) {
+    public boolean save(Reference reference) {
         if (reference.validate()) {
             try {
                 refRepo.save(reference);
@@ -38,10 +41,9 @@ public class ReferenceService {
                 reference.generateKey();
                 refRepo.save(reference);
             }
-            return "redirect:/references";
-        } else {
-            return "redirect:/references/create/" + reference.getRefType();
+            return true;
         }
+        return false;
     }
 
     public void delete(Long id) {
@@ -52,16 +54,36 @@ public class ReferenceService {
     public HttpEntity<byte[]> makeFile(String filename) {
         byte[] data = generateBibtex().getBytes();
         HttpHeaders header = makeHeader(filename, data);
-        return new HttpEntity<byte[]>(data, header  );
+        return new HttpEntity<byte[]>(data, header);
 
     }
 
     private HttpHeaders makeHeader(@PathVariable String filename, byte[] data) {
         HttpHeaders header = new HttpHeaders();
         header.setContentType(MediaType.TEXT_PLAIN);
-        header.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename="+filename+".bib");
+        header.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + filename + ".bib");
         header.setContentLength(data.length);
         return header;
+    }
+
+    public HashMap<String, List<Reference>> findAllByRefType() {
+        HashMap<String, List<Reference>> map = new HashMap<>();
+        map.put("articleReferences", refRepo.findByRefType("article"));
+        map.put("articleReferences", refRepo.findByRefType("article"));
+        map.put("bookReferences", refRepo.findByRefType("book"));
+        map.put("bookletReferences", refRepo.findByRefType("booklet"));
+        map.put("conferenceReferences", refRepo.findByRefType("conference"));
+        map.put("inbookReferences", refRepo.findByRefType("inbook"));
+        map.put("incollectionReferences", refRepo.findByRefType("incollection"));
+        map.put("inproceedingsReferences", refRepo.findByRefType("inproceedings"));
+        map.put("manualReferences", refRepo.findByRefType("manual"));
+        map.put("mastersthesisReferences", refRepo.findByRefType("mastersthesis"));
+        map.put("miscReferences", refRepo.findByRefType("misc"));
+        map.put("phdthesisReferences", refRepo.findByRefType("phdthesis"));
+        map.put("proceedingsReferences", refRepo.findByRefType("proceedings"));
+        map.put("techreportReferences", refRepo.findByRefType("techreport"));
+        map.put("unpublishedReferences", refRepo.findByRefType("unpublished"));
+        return map;
     }
 
 }
