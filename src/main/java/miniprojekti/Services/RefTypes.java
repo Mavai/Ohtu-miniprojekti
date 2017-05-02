8b21ad309587974ae231f63143f333365f599151
Kalle Perkoila <kalle.perkoila@helsinki.fi>
2017-05-02 19:29:56 +0300

    RefTypes säätöä

--- src/main/java/miniprojekti/Services/RefTypes.java
+++ src/main/java/miniprojekti/Services/RefTypes.java
@@ -7,7 +7,7 @@ public class RefTypes {
  private HashMap<String, HashMap> types;
 
  public RefTypes() {
-    this.types = new HashMap<String, HashMap>();
+    this.types = new HashMap<>();
     types.put("article", generateArticle());
     types.put("book", generateBook());
     types.put("booklet", generateBooklet());
@@ -24,8 +24,8 @@ public class RefTypes {
     types.put("unpublished", generateUnpublished());
  }
 
-private generateBook() {
-    HashMap<String, Boolean[]> map;
+private HashMap generateBook() {
+    HashMap<String, Boolean[]> map = new HashMap();
     map.put("name", new Boolean[] {true, true});
     map.put("author", new Boolean[] {true, true});
     map.put("title", new Boolean[] {true, true});
@@ -51,10 +51,11 @@ private generateBook() {
     map.put("school", new Boolean[] {true, true});
     map.put("booktitle", new Boolean[] {true, true});
     map.put("type", new Boolean[] {true, true});
+    return map;
 }
 
-private generateArticle() {
-    HashMap<String, Boolean[]> map;
+private HashMap generateArticle() {
+    HashMap<String, Boolean[]> map = new HashMap();
     map.put("name", new Boolean[] {true, true});
     map.put("author", new Boolean[] {true, true});
     map.put("title", new Boolean[] {true, true});
@@ -80,10 +81,11 @@ private generateArticle() {
     map.put("school", new Boolean[] {true, true});
     map.put("booktitle", new Boolean[] {true, true});
     map.put("type", new Boolean[] {true, true});
+    return map;
 }
 
-private generateBooklet() {
-    HashMap<String, Boolean[]> map;
+private HashMap generateBooklet() {
+    HashMap<String, Boolean[]> map = new HashMap();
     map.put("name", new Boolean[] {true, true});
     map.put("author", new Boolean[] {true, true});
     map.put("title", new Boolean[] {true, true});
@@ -109,10 +111,11 @@ private generateBooklet() {
     map.put("school", new Boolean[] {true, true});
     map.put("booktitle", new Boolean[] {true, true});
     map.put("type", new Boolean[] {true, true});
+    return map;
 }
 
-private generateConference() {
-    HashMap<String, Boolean[]> map;
+private HashMap generateConference() {
+    HashMap<String, Boolean[]> map = new HashMap();
     map.put("name", new Boolean[] {true, true});
     map.put("author", new Boolean[] {true, true});
     map.put("title", new Boolean[] {true, true});
@@ -138,10 +141,11 @@ private generateConference() {
     map.put("school", new Boolean[] {true, true});
     map.put("booktitle", new Boolean[] {true, true});
     map.put("type", new Boolean[] {true, true});
+    return map;
 }
 
-private generateInbook() {
-    HashMap<String, Boolean[]> map;
+private HashMap generateInbook() {
+    HashMap<String, Boolean[]> map = new HashMap();
     map.put("name", new Boolean[] {true, true});
     map.put("author", new Boolean[] {true, true});
     map.put("title", new Boolean[] {true, true});
@@ -167,10 +171,11 @@ private generateInbook() {
     map.put("school", new Boolean[] {true, true});
     map.put("booktitle", new Boolean[] {true, true});
     map.put("type", new Boolean[] {true, true});
+    return map;
 }
 
-private generateIncollection() {
-    HashMap<String, Boolean[]> map;
+private HashMap generateIncollection() {
+    HashMap<String, Boolean[]> map = new HashMap();
     map.put("name", new Boolean[] {true, true});
     map.put("author", new Boolean[] {true, true});
     map.put("title", new Boolean[] {true, true});
@@ -196,10 +201,11 @@ private generateIncollection() {
     map.put("school", new Boolean[] {true, true});
     map.put("booktitle", new Boolean[] {true, true});
     map.put("type", new Boolean[] {true, true});
+    return map;
 }
 
-private generateInproceedings() {
-    HashMap<String, Boolean[]> map;
+private HashMap generateInproceedings() {
+    HashMap<String, Boolean[]> map = new HashMap();
     map.put("name", new Boolean[] {true, true});
     map.put("author", new Boolean[] {true, true});
     map.put("title", new Boolean[] {true, true});
@@ -225,10 +231,11 @@ private generateInproceedings() {
     map.put("school", new Boolean[] {true, true});
     map.put("booktitle", new Boolean[] {true, true});
     map.put("type", new Boolean[] {true, true});
+    return map;
 }
 
-private generateManual() {
-    HashMap<String, Boolean[]> map;
+private HashMap generateManual() {
+    HashMap<String, Boolean[]> map = new HashMap();
     map.put("name", new Boolean[] {true, true});
     map.put("author", new Boolean[] {true, true});
     map.put("title", new Boolean[] {true, true});
@@ -254,10 +261,11 @@ private generateManual() {
     map.put("school", new Boolean[] {true, true});
     map.put("booktitle", new Boolean[] {true, true});
     map.put("type", new Boolean[] {true, true});
+    return map;
 }
 
-private generateMastersthesis() {
-    HashMap<String, Boolean[]> map;
+private HashMap generateMastersthesis() {
+    HashMap<String, Boolean[]> map = new HashMap();
     map.put("name", new Boolean[] {true, true});
     map.put("author", new Boolean[] {true, true});
     map.put("title", new Boolean[] {true, true});
@@ -283,10 +291,11 @@ private generateMastersthesis() {
     map.put("school", new Boolean[] {true, true});
     map.put("booktitle", new Boolean[] {true, true});
     map.put("type", new Boolean[] {true, true});
+    return map;
 }
 
-private generateMisc() {
-    HashMap<String, Boolean[]> map;
+private HashMap generateMisc() {
+    HashMap<String, Boolean[]> map = new HashMap();
     map.put("name", new Boolean[] {true, true});
     map.put("author", new Boolean[] {true, true});
     map.put("title", new Boolean[] {true, true});
@@ -312,10 +321,11 @@ private generateMisc() {
     map.put("school", new Boolean[] {true, true});
     map.put("booktitle", new Boolean[] {true, true});
     map.put("type", new Boolean[] {true, true});
+    return map;
 }
 
-private generatePhdthesis() {
-    HashMap<String, Boolean[]> map;
+private HashMap generatePhdthesis() {
+    HashMap<String, Boolean[]> map = new HashMap();
     map.put("name", new Boolean[] {true, true});
     map.put("author", new Boolean[] {true, true});
     map.put("title", new Boolean[] {true, true});
@@ -341,10 +351,11 @@ private generatePhdthesis() {
     map.put("school", new Boolean[] {true, true});
     map.put("booktitle", new Boolean[] {true, true});
     map.put("type", new Boolean[] {true, true});
+    return map;
 }
 
-private generateProceedings() {
-    HashMap<String, Boolean[]> map;
+private HashMap generateProceedings() {
+    HashMap<String, Boolean[]> map = new HashMap();
     map.put("name", new Boolean[] {true, true});
     map.put("author", new Boolean[] {true, true});
     map.put("title", new Boolean[] {true, true});
@@ -370,39 +381,41 @@ private generateProceedings() {
     map.put("school", new Boolean[] {true, true});
     map.put("booktitle", new Boolean[] {true, true});
     map.put("type", new Boolean[] {true, true});
+    return map;
 }
 
-private generateTechreport() {
-    HashMap<String, Boolean[]> map;
+private HashMap generateTechreport() {
+    HashMap<String, Boolean[]> map = new HashMap();
     map.put("name", new Boolean[] {true, true});
     map.put("author", new Boolean[] {true, true});
     map.put("title", new Boolean[] {true, true});
-    map.put("journal", new Boolean[] {false, false});
+    map.put("journal", new Boolean[] {true, true});
     map.put("year", new Boolean[] {true, true});
-    map.put("volume", new Boolean[] {false, false});
-    map.put("number", new Boolean[] {true, false});
-    map.put("pages", new Boolean[] {false, false});
-    map.put("month", new Boolean[] {true, false});
-    map.put("note", new Boolean[] {true, false});
-    map.put("key", new Boolean[] {true, false});
-    map.put("publisher", new Boolean[] {false, false});
-    map.put("series", new Boolean[] {false, false});
-    map.put("address", new Boolean[] {true, false});
-    map.put("edition", new Boolean[] {false, false});
-    map.put("annote", new Boolean[] {false, false});
-    map.put("chapter", new Boolean[] {false, false});
-    map.put("crossref", new Boolean[] {false, false});
-    map.put("editor", new Boolean[] {false, false});
-    map.put("howpublished", new Boolean[] {false, false});
+    map.put("volume", new Boolean[] {true, true});
+    map.put("number", new Boolean[] {true, true});
+    map.put("pages", new Boolean[] {true, true});
+    map.put("month", new Boolean[] {true, true});
+    map.put("note", new Boolean[] {true, true});
+    map.put("key", new Boolean[] {true, true});
+    map.put("publisher", new Boolean[] {true, true});
+    map.put("series", new Boolean[] {true, true});
+    map.put("address", new Boolean[] {true, true});
+    map.put("edition", new Boolean[] {true, true});
+    map.put("annote", new Boolean[] {true, true});
+    map.put("chapter", new Boolean[] {true, true});
+    map.put("crossref", new Boolean[] {true, true});
+    map.put("editor", new Boolean[] {true, true});
+    map.put("howpublished", new Boolean[] {true, true});
     map.put("institution", new Boolean[] {true, true});
-    map.put("organization", new Boolean[] {false, false});
-    map.put("school", new Boolean[] {false, false});
-    map.put("booktitle", new Boolean[] {false, false});
-    map.put("type", new Boolean[] {true, false});
+    map.put("organization", new Boolean[] {true, true});
+    map.put("school", new Boolean[] {true, true});
+    map.put("booktitle", new Boolean[] {true, true});
+    map.put("type", new Boolean[] {true, true});
+    return map;
 }
 
-private generateUnpublished() {
-    HashMap<String, Boolean[]> map;
+private HashMap generateUnpublished() {
+    HashMap<String, Boolean[]> map = new HashMap();
     map.put("name", new Boolean[] {true, true});
     map.put("author", new Boolean[] {true, true});
     map.put("title", new Boolean[] {true, true});
@@ -428,6 +441,7 @@ private generateUnpublished() {
     map.put("school", new Boolean[] {false, false});
     map.put("booktitle", new Boolean[] {false, false});
     map.put("type", new Boolean[] {false, false});
+    return map;
 }
 
 
