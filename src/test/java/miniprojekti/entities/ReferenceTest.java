0443442de490ad6ebd697ad404198998602a92ef
hanranti <hanranti@cs.helsinki.fi>
2017-05-03 14:25:21 +0300

    generateKeyTestit ilman refServicea

--- src/test/java/miniprojekti/entities/ReferenceTest.java
+++ src/test/java/miniprojekti/entities/ReferenceTest.java
@@ -73,24 +73,27 @@ public class ReferenceTest {
     @Test
     public void referenceNameIsGeneratedCorrectlyWhenTitlesSizeIsOver4() {
         Reference ref = new Reference("author:authorName1", "title:theTitle", "refType:article");
-        refService.save(ref);
-        String bibtex = "@article{theTi" + ref.getId() + ",\n author    = \"authorName1\",\n title     = \"theTitle\"\n}\n\n";
+        ref.setId(888L);
+        ref.generateKey();
+        String bibtex = "@article{theTi888,\n author    = \"authorName1\",\n title     = \"theTitle\"\n}\n\n";
         assertEquals(bibtex, ref.getBibtex());
     }
 
     @Test
     public void referenceNameIsGeneratedCorrectlyWhenTitlesSizeIs4() {
         Reference ref = new Reference("author:authorName2", "title:qwert", "refType:article");
-        refService.save(ref);
-        String bibtex = "@article{qwert" + ref.getId() + ",\n author    = \"authorName2\",\n title     = \"qwert\"\n}\n\n";
+        ref.setId(999L);
+        ref.generateKey();
+        String bibtex = "@article{qwert999,\n author    = \"authorName2\",\n title     = \"qwert\"\n}\n\n";
         assertEquals(bibtex, ref.getBibtex());
     }
 
     @Test
     public void referenceNameIsGeneratedCorrectlyWhenTitlesSizeIsLessThan4() {
         Reference ref = new Reference("author:authorName3", "title:asd", "refType:article");
-        refService.save(ref);
-        String bibtex = "@article{asd" + ref.getId() + ",\n author    = \"authorName3\",\n title     = \"asd\"\n}\n\n";
+        ref.setId(777L);
+        ref.generateKey();
+        String bibtex = "@article{asd777,\n author    = \"authorName3\",\n title     = \"asd\"\n}\n\n";
         assertEquals(bibtex, ref.getBibtex());
     }
 }
